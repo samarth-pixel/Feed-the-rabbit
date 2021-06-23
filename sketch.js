0a4590e2f9a613e60bdb8c74b65cdc1cc69530b9
samarth-pixel <83510775+samarth-pixel@users.noreply.github.com>
2021-06-22 21:22:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple, appleImg;
+var leaves, leavesImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leavesImg = loadImage("leaves.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  rabbit.x = World.mouseX;
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  var select_sprites = Math.round(random(1,2));
+
+  if (frameCount % 80 == 0) {
+    if (select_sprites == 1) {
+      createApples();
+    }
+    else {
+      createLeaves();
+    }
+  }
+  drawSprites();
+}
+function createApples() {
+  apple = createSprite(random(50,350), 40, 10, 10);
+  apple.addImage(appleImg);
+  apple.scale = 0.1;
+  apple.velocityY = 5;
+  apple.lifetime = 300;
+}
+
+function createLeaves() {
+  leaves = createSprite(random(50,350), 40, 10, 10);
+  leaves.addImage(leavesImg);
+  leaves.scale = 0.05;
+  leaves.velocityY = 5;
+  leaves.lifetime = 300;
+}
